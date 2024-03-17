f59040290cb7db564ddae93cdf7af2f5056545e6
Rasim ALTUNTAŞ <rasim.altuntas@amperino.com>
2024-03-17 15:06:17 +0300

    Fixed the response model of Race List

--- F1Service/Model/RaceListModel.swift
+++ F1Service/Model/RaceListModel.swift
@@ -5,55 +5,46 @@
 
 import Foundation
 
-// MARK: - Welcome
 struct RaceListModel: Codable {
     let mrData: MRData
-
+    
     enum CodingKeys: String, CodingKey {
         case mrData = "MRData"
     }
     
-    // MARK: - MRData
     struct MRData: Codable {
-        let xmlns: String
-        let series: String
-        let url: String
-        let limit, offset, total: String
+        let xmlns,series,url,limit,offset,total: String
         let raceTable: RaceTable
-
+        
         enum CodingKeys: String, CodingKey {
             case xmlns, series, url, limit, offset, total
             case raceTable = "RaceTable"
+            
         }
     }
-
-    // MARK: - RaceTable
+    
     struct RaceTable: Codable {
         let season: String
         let races: [Race]
-
-        enum CodingKeys: String, CodingKey {
+        
+        enum CodingKeys: String,CodingKey {
             case season
             case races = "Races"
         }
     }
-
-    // MARK: - Race
+    
     struct Race: Codable {
-        let season, round: String
-        let url: String
-        let raceName: String
+        let season, round, url, raceName, date, time : String
         let circuit: Circuit
-        let date, time: String
-        let firstPractice, secondPractice: FirstPractice
-        let thirdPractice: FirstPractice?
-        let qualifying: FirstPractice
-        let sprint: FirstPractice?
-
+        let firstPractice: Schedule
+        let secondPractice: Schedule
+        let thirdPractice: Schedule?
+        let qualifying: Schedule
+        let sprint: Schedule?
+        
         enum CodingKeys: String, CodingKey {
-            case season, round, url, raceName
+            case season, round, url, raceName, date, time
             case circuit = "Circuit"
-            case date, time = ""
             case firstPractice = "FirstPractice"
             case secondPractice = "SecondPractice"
             case thirdPractice = "ThirdPractice"
@@ -61,30 +52,30 @@ struct RaceListModel: Codable {
             case sprint = "Sprint"
         }
     }
-
-    // MARK: - Circuit
+    
     struct Circuit: Codable {
-        let circuitID: String
-        let url: String
-        let circuitName: String
+        let circuitId, url, circuitName: String
         let location: Location
-
+        
         enum CodingKeys: String, CodingKey {
-            case circuitID = "circuitId"
-            case url, circuitName
+            case circuitId, url, circuitName
             case location = "Location"
         }
     }
-
-    // MARK: - Location
+    
+    struct Schedule: Codable {
+        let date,time: String
+        
+        enum CodingKeys: String, CodingKey {
+            case date,time
+        }
+    }
+    
     struct Location: Codable {
         let lat, long, locality, country: String
+        
+        enum CodingKeys: String, CodingKey {
+            case lat, long, locality, country
+        }
     }
-
-    // MARK: - FirstPractice
-    struct FirstPractice: Codable {
-        let date, time: String
-    }
-
 }
-
